cb75c06fa9b03c0929207e3b9009d66993694a78
cvasfi <yunus.ec@gmail.com>
2018-02-08 19:47:41 +0100

    freeze parameter support for tl training

--- traffic_light_classification/merge_lst.py
+++ traffic_light_classification/merge_lst.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-
+import random
 parser = argparse.ArgumentParser(description='Train a Single-shot detection network')
 
 parser.add_argument('--seqdir', dest='seqdir', help='directory containing all the corrected sequences',
@@ -9,13 +9,17 @@ parser.add_argument('--seqdir', dest='seqdir', help='directory containing all th
 parser.add_argument('--labeldir', dest='labeldir', help='directory containing all the corrected sequences',
                     default="../../../datasets/apex_tl/labels", type=str)
 parser.add_argument('--output', dest='output', help='directory containing all the corrected sequences',
-                    default="apex_combined.lst", type=str)
+                    default="apex_combined_0602", type=str)
+parser.add_argument('--valsplit', dest='valsplit', help='split ratio',
+                    default=0.2, type=float)
 pargs = parser.parse_args()
 
 seqs=os.listdir(pargs.seqdir)
 
-lst_data=""
-global_idx=0
+train_lst=""
+val_lst=""
+val_idx=0
+lines=[]
 for seq in seqs:
     path='images/'+seq+"/"
     with open(os.path.join(pargs.labeldir,seq+".lst"), 'r') as f:
@@ -23,8 +27,24 @@ for seq in seqs:
             words=line.split('\t')
             #print words
 
-            lst_data+=str(global_idx)+"\t"+words[1]+"\t"+path + os.path.basename(words[2])
-            global_idx+=1
+            lines.append("\t"+words[1]+"\t"+path + os.path.basename(words[2]))
 
-with open(pargs.output, "w") as text_file:
-    text_file.write(lst_data)
+
+num_samples=len(lines)
+random.shuffle(lines)
+train_num=int(num_samples*(1-pargs.valsplit))
+val_num=num_samples-train_num
+
+for i in range(train_num):
+    train_lst+=str(i)+lines[i]
+
+
+with open(pargs.output+"_train.lst", "w") as text_file:
+    text_file.write(train_lst)
+
+for i in range(train_num,num_samples):
+    val_lst += str(val_idx) + lines[i]
+    val_idx+=1
+
+with open(pargs.output + "_val.lst", "w") as text_file:
+    text_file.write(val_lst)
--- traffic_light_classification/train_nexar.py
+++ traffic_light_classification/train_nexar.py
@@ -5,10 +5,11 @@ import sys
 sys.path.append("../symbol")
 import resnet
 import mobilenet
-
+import densenet
+import re
 import mxnet as mx
 
-parser = argparse.ArgumentParser(description='Train a Single-shot detection network')
+parser = argparse.ArgumentParser(description='Train a traffic light classifier')
 parser.add_argument('--resume', dest='resume', help='resume from epoch',
                     default=-1, type=int)
 parser.add_argument('--prefix', dest='prefix', help='model prefix',
@@ -19,7 +20,14 @@ parser.add_argument('--train_rec', dest='train_rec', help='train file to use',
 
 parser.add_argument('--val_rec', dest='val_rec', help='val file to use',
                     default="../data/nexar/nexar_val_noisy.rec", type=str)
+parser.add_argument('--lr', dest='lr', help='learning rate',
+                    default=0.005, type=float)
 
+parser.add_argument('--batch-size', dest='batch_size', help='batch size',
+                    default=64, type=int)
+
+parser.add_argument('--freeze', dest='freeze', help='freeze some layers for finetuning',
+                    default=False, type=bool)
 
 parser.add_argument('--net', dest='net', help='val file to use',
                     default="resnet18", type=str)
@@ -46,43 +54,62 @@ def get_lr_scheduler(learning_rate, lr_refactor_step, lr_refactor_ratio,
 
 if __name__ == '__main__':
     # download data
-    pargs = parser.parse_args()
-    sym = resnet.get_symbol(4,18,"3,32,32") if(pargs.net=="resnet18") else mobilenet.get_symbol(4,alpha=0.50)
 
+    patterns={}
+    patterns['densenet30']= "^(conv0_|DBstage1_unit._conv1_|DBstage2_unit._conv1_).*"
+    patterns ['resnet18']="^(conv0_|stage1_unit._conv._|stage2_unit._conv._|stage3_unit._conv._|stage4_unit1_conv._).*"
+
+    pargs = parser.parse_args()
+
+    if(pargs.resume>-1):
+        print("resuming from %d"%pargs.resume)
+        sym, args, auxs = mx.model.load_checkpoint(pargs.prefix, pargs.resume)
+        epoch=pargs.resume
+    else:
+        if(pargs.net=="resnet18"):
+            sym=resnet.get_symbol(4,18,"3,32,32")
+        elif (pargs.net=="mobilenet050"):
+            sym=mobilenet.get_symbol(4,alpha=0.50)
+        elif (pargs.net=="densenet30"):
+            sym=densenet.DenseNet(units=[9,9,9], num_stage=3, growth_rate=12, num_class=4, data_type="nexar",
+                              bottle_neck=False)
+        args = None
+        auxs = None
+        epoch=0
+
+    if(pargs.freeze):
+        re_prog = re.compile(patterns[pargs.net])
+        fixed_param_names = [name for name in sym.list_arguments() if re_prog.match(name)]
+        if fixed_param_names:
+            print("Freezed parameters: [" + ','.join(fixed_param_names) + ']')
+    else:
+        fixed_param_names=None
 
     logging.getLogger().setLevel(logging.DEBUG)
 
     train_iter = mx.io.ImageRecordIter(
       path_imgrec=pargs.train_rec, data_name="data", label_name="softmax_label",
-      batch_size=128, data_shape=(3,32,32), shuffle=True, rand_crop=True, rand_mirror=True)
+      batch_size=pargs.batch_size, data_shape=(3,32,32), shuffle=True, rand_crop=True, rand_mirror=True)
 
     valid_iter = mx.io.ImageRecordIter(
       path_imgrec=pargs.val_rec, data_name="data", label_name="softmax_label",
-    batch_size=128, data_shape=(3,32,32))
+    batch_size=pargs.batch_size, data_shape=(3,32,32))
 
     batch_end_callback = mx.callback.Speedometer(train_iter.batch_size, frequent=100)
     epoch_end_callback = mx.callback.do_checkpoint(pargs.prefix)
 
-    mod = mx.mod.Module(symbol=sym, context=(mx.gpu(0)))
+    mod = mx.mod.Module(symbol=sym, context=(mx.gpu(0)), fixed_param_names=fixed_param_names)
     mod.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
 
-    if(pargs.resume>-1):
-        print("resuming from %d"%pargs.resume)
-        _, args, auxs = mx.model.load_checkpoint(pargs.prefix, pargs.resume)
-        epoch=pargs.resume
-    else:
-        print("training from scratch")
-        args = None
-        auxs = None
-        epoch=0
 
-    learning_rate, lr_scheduler = get_lr_scheduler(0.005, '80, 160',
+
+    learning_rate, lr_scheduler = get_lr_scheduler(pargs.lr, '80, 160',
                                                    0.1, 48638, 128, epoch)
 
-    optimizer_params={'learning_rate':learning_rate,
+    optimizer_params={'learning_rate':pargs.lr,
                       'momentum':0.9,
                       'wd':0.0005,
-                      'lr_scheduler':lr_scheduler,
+                      #'lr_scheduler':lr_scheduler,
                       'clip_gradient':None,
                       'rescale_grad': 1.0 }
 
