e259a2ac364e4509696af39faa9db477bbd2aa66
cvasfi <yunus.ec@gmail.com>
2018-08-28 00:03:11 +0200

    quick hack for time benchmark

--- time_benchmark.py
+++ time_benchmark.py
@@ -10,6 +10,8 @@ parser.add_argument('--epoch', dest='epoch', help='epoch of trained model',
 parser.add_argument('--prefix', dest='prefix', help='trained model prefix',
                     default="car_models/deploy_resnet18_2c_v2",
                     type=str)
+parser.add_argument('--num_classes', dest='num_classes', help='number of classes',
+                    default=5, type=int)
 parser.add_argument('--image', dest='image', help='image path',
                     default="data/demo/street.jpg",
                     type=str)
@@ -21,7 +23,7 @@ pargs = parser.parse_args()
 
 sum=0.0
 
-detector=object_detector.object_detector(prefix=pargs.prefix, epoch=pargs.epoch, conf_thresh=0.5, device=pargs.ctx)
+detector=object_detector.object_detector(prefix=pargs.prefix, epoch=pargs.epoch, classes= range(pargs.num_classes), conf_thresh=0.5, device=pargs.ctx)
 im=cv2.imread("data/demo/street.jpg")
 loop=1000
 
